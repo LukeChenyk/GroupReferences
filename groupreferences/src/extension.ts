a66f5bc6441e5fd8ac6e516a13d2f83368753741
yukuan.chen <yukuan.chen@happyelements.com>
2023-09-22 10:56:50 +0800

    删除没用的代码

--- groupreferences/src/extension.ts
+++ groupreferences/src/extension.ts
@@ -11,25 +11,6 @@ export function activate(context: vscode.ExtensionContext)
 
 	console.log('Congratulations, your extension "groupreferences" is now active!');
 
-	// let disposable = vscode.commands.registerCommand('groupreferences.helloWorld', () =>
-	// {
-	// 	// The code you place here will be executed every time your command is executed
-	// 	// Display a message box to the user
-	// 	vscode.window.showInformationMessage('Hello World from GroupReferences!I am a new extension of Yuk!');
-	// });
-
-	// context.subscriptions.push(disposable);
-
-
-	let lowerCase = vscode.commands.registerCommand('extension.toLowerCase', toLowerCase);
-	let upperCase = vscode.commands.registerCommand('extension.toUpperCase', toUpperCase);
-	context.subscriptions.push(lowerCase);
-	context.subscriptions.push(upperCase);
-
-	// let referenceItemClkCmd = vscode.commands.registerCommand('groupreferences.referenceItemClk', OnReferenceItemClkCmd);
-	// context.subscriptions.push(referenceItemClkCmd);
-
-
 	//注册侧边栏面板的实现
 	const readDataProvider = new sidebar.TreeProvider(false);
 	vscode.window.registerTreeDataProvider("sidebar_groupreferences_id1", readDataProvider);
@@ -39,17 +20,6 @@ export function activate(context: vscode.ExtensionContext)
 
 
 	// //注册命令 
-	// vscode.commands.registerCommand("sidebar_test_id1.openChild", args =>
-	// {
-	// 	vscode.window.showInformationMessage(args);
-	// });
-
-	// var provider = new Provider()
-
-	// var providerRegistrations = vscode.Disposable.from(
-	// 	vscode.workspace.registerTextDocumentContentProvider(Provider.scheme, provider),
-	// 	vscode.languages.registerDocumentLinkProvider({ scheme: Provider.scheme }, provider));
-
 	var commandRegistration = vscode.commands.registerTextEditorCommand('groupreferences.findAllReferences', function (editor: vscode.TextEditor)
 	{
 		var uri = encodeLocation(editor.document.uri, editor.selection.active);
@@ -73,15 +43,7 @@ export function activate(context: vscode.ExtensionContext)
 				writeDataProvider.setDataSources(dataSources);
 			})
 		});
-
-
-		// var viewColumn = editor.viewColumn ? editor.viewColumn + 1 : undefined;
-		// return vscode.workspace.openTextDocument(uri).then((doc: vscode.TextDocument) =>
-		// {
-		// 	return vscode.window.showTextDocument(doc, viewColumn);
-		// });
 	});
-	// context.subscriptions.push(provider, commandRegistration, providerRegistrations);
 
 	context.subscriptions.push(commandRegistration);
 
@@ -133,7 +95,6 @@ function processDataSources(dataSources: sidebar.LocationSource[])
 
 				vscode.commands.executeCommand('vscode.executeDocumentHighlights', uri, loc.range.start).then((args: any) =>
 				{
-
 					let documentHighlight: vscode.DocumentHighlight[] = args;
 
 					for (let I = 0; I < documentHighlight.length; I++)
@@ -160,52 +121,6 @@ function processDataSources(dataSources: sidebar.LocationSource[])
 			});
 		}
 	});
-}
-
-
-function toLowerCase()
-{
-	toLowerCaseOrUpperCase('toLowerCase');
-}
-
-function toUpperCase()
-{
-	toLowerCaseOrUpperCase('toUpperCase');
-}
-
-//转小写
-function toLowerCaseOrUpperCase(command: string)
-{
-	//获取activeTextEditor
-	const editor = vscode.window.activeTextEditor;
-	if (editor)
-	{
-		const document = editor.document;
-		const selection = editor.selection;
-		//获取选中单词文本
-		const word = document.getText(selection);
-		//文本转大小写
-		const newWord = command === 'toLowerCase' ? word.toLowerCase() : word.toUpperCase();
-		//替换原来文本
-		editor.edit((editBuilder) =>
-		{
-			editBuilder.replace(selection, newWord);
-		});
-	}
-}
-
-function OnReferenceItemClkCmd(uri: vscode.Uri)
-{
-	// const showDocOptions = {
-	// 	preserveFocus: false,
-	// 	preview: false,
-	// 	viewColumn: 1,
-
-	// 	// replace with your line_number's
-	// 	selection: new vscode.Range(314, 0, 314, 0)
-	// };
-
-	// let doc = await vscode.window.showTextDocument(setting, showDocOptions);
 }
 
 // This method is called when your extension is deactivated
